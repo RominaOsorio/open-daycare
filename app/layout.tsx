a44a8357d1ab00e5c80ac2acca0907268055243d
Romina Osorio <r.osorio.marmolejo@gmail.com>
2026-09-06 20:04:35 -0300

    fix(layout): suprimir warning de hidratación por atributo de extensión

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="es"
       className={`${fredoka.variable} ${nunito.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
